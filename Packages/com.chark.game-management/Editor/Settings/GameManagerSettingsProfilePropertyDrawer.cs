6c3c8733040d0a937f4de527d706f00048583834
Edvinas <edvinas108@gmail.com>
2023-10-06 17:21:25 +0300

    Add drawer to better visualize profiles

---
+++ Packages/com.chark.game-management/Editor/Settings/GameManagerSettingsProfilePropertyDrawer.cs
@@ -0,0 +1,46 @@
+﻿using CHARK.GameManagement.Settings;
+using UnityEditor;
+using UnityEngine;
+
+namespace CHARK.GameManagement.Editor.Settings
+{
+    [CustomPropertyDrawer(typeof(GameManagerSettingsProfile))]
+    internal sealed class GameManagerSettingsProfilePropertyDrawer : PropertyDrawer
+    {
+        private const float ToggleWidth = 16f;
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var profile = property.objectReferenceValue as GameManagerSettingsProfile;
+            EditorGUI.BeginProperty(position, label, property);
+
+            var initialColor = GUI.backgroundColor;
+            if (profile && profile.IsActiveProfile)
+            {
+                GUI.backgroundColor = Color.green;
+            }
+
+            EditorGUI.BeginChangeCheck();
+
+            var togglePosition = position;
+            togglePosition.width = ToggleWidth;
+
+            var isActiveProfileNew = profile
+                ? EditorGUI.Toggle(togglePosition, profile.IsActiveProfile)
+                : EditorGUI.Toggle(togglePosition, false);
+
+            if (EditorGUI.EndChangeCheck() && profile)
+            {
+                profile.IsActiveProfile = isActiveProfileNew;
+            }
+
+            var propertyPosition = position;
+            propertyPosition.width -= ToggleWidth;
+            propertyPosition.x += ToggleWidth;
+
+            EditorGUI.PropertyField(propertyPosition, property, new GUIContent("Profile"));
+            GUI.backgroundColor = initialColor;
+
+            EditorGUI.EndProperty();
+        }
+    }
+}
