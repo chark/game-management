bbcaafb07de12c72eed495a5c0b5f5f4add5d5d4
Edvinas Danevičius <edvinas108@gmail.com>
2025-07-25 01:42:38 +0300

    Add test for reading a resource from StreamingAssets

---
+++ Packages/com.chark.game-management/Tests/Editor/DefaultResourceLoaderTest.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using CHARK.GameManagement.Assets;
+using CHARK.GameManagement.Serialization;
+using NUnit.Framework;
+
+namespace CHARK.GameManagement.Tests.Editor
+{
+    internal sealed class DefaultResourceLoaderTest
+    {
+        private DefaultResourceLoader resourceLoader;
+
+        [SetUp]
+        public void SetUp()
+        {
+            resourceLoader = new DefaultResourceLoader(DefaultSerializer.Instance);
+        }
+
+        [Test]
+        public async Task ShouldReadFileFromStreamingAssets()
+        {
+            // When
+            var stream = await resourceLoader.ReadResourceStreamAsync("TestData.txt");
+            var length = stream.Length;
+
+            string text;
+            using (var reader = new StreamReader(stream))
+            {
+                text = await reader.ReadToEndAsync();
+            }
+
+            // Then
+            Assert.Greater(length, 0);
+            Assert.AreEqual(text, "hello");
+        }
+    }
+}
